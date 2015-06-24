d24abda9ce15eedd45ef840ca44520e320fdbbff
Greg Thompson <thompson.glowe@gmail.com>
2015-06-24 10:38:38 -0500

    Use asset.equal() for equality tests. (assert() is truthy)

--- test/index.js
+++ test/index.js
@@ -11,32 +11,31 @@ var basscssGridInfo = getModuleInfo('basscss-grid');
 describe('get-module-info', function() {
 
   it('should be an object', function() {
-    assert(typeof postcssInfo, 'object');
+    assert.equal(typeof postcssInfo, 'object');
   });
 
   it('should have a name', function() {
-    assert(typeof postcssInfo.name, 'string');
+    assert.equal(typeof postcssInfo.name, 'string');
   });
 
   it('should have a version', function() {
-    assert(typeof postcssInfo.version, 'string');
+    assert.equal(typeof postcssInfo.version, 'string');
   });
 
   it('should have a title', function() {
-    assert(typeof postcssInfo.title, 'string');
+    assert.equal(typeof postcssInfo.title, 'string');
   });
 
   it('should have a readme', function() {
-    assert(typeof postcssInfo.readme, 'string');
+    assert.equal(typeof postcssInfo.readme, 'string');
   });
 
   it('should get css', function() {
-    assert(typeof basscssGridInfo.css, 'string');
+    assert.equal(typeof basscssGridInfo.css, 'string');
   });
 
   it('should get css stats', function() {
-    assert(typeof basscssGridInfo.stats, 'object');
+    assert.equal(typeof basscssGridInfo.stats, 'object');
   });
 
 });
-
